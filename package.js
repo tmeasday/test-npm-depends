4095aaf551ca62218b1d7334f0b32418c74bf591
Tom Coleman <tom@thesnail.org>
2016-04-25 12:29:25 -0700

    repro

---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: 'test-npm-depends',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2');
+  api.use('ecmascript');
+  api.addFiles('test-npm-depends.js');
+  Npm.depends({
+    moment: '2.13.0'
+  });
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('test-npm-depends');
+  api.addFiles('test-npm-depends-tests.js');
+});
---
+++ test-npm-depends-tests.js
@@ -0,0 +1,5 @@
+// Write your tests here!
+// Here is an example.
+Tinytest.add('test-npm-depends - example', function (test) {
+  test.equal(packageName, "test-npm-depends");
+});
